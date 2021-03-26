04ca03832bec252d48d648e3dbcc3e7930e9a8e7
LeeJan-0826 <leejan0826@163.com>
2021-03-26 15:21:27 +0800

    作业

---
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,22 @@
+﻿#include<iostream>
+using namespace std;
+int factorial(int _n, int res)
+{
+	if (_n == 1 || _n == 0)
+		return res;
+	else
+		return factorial(_n - 1, _n * res);
+}
+int main(int argc, char* argv[])
+{
+	std::cout << "请输入一个整数：" << endl;
+	int input_nums = 0;
+	cin >> input_nums;
+	int ans = factorial(input_nums, 1);
+	if (ans<0 || ans>INT_MAX)
+		std::cout << "输入数字过大，已无法计算" << endl;
+	else
+		std::cout << input_nums << "的阶乘为" << ans << endl;
+	system("pause");
+	return 0;
+}
